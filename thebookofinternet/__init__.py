d53dac7d3463f881d7b7e23e4c38bb0844216434
Pilsang Kim <pilsang0512@gmail.com>
2020-09-07 22:56:34 +0900

    get db auth from environment

--- thebookofinternet/__init__.py
+++ thebookofinternet/__init__.py
@@ -1,10 +1,12 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:rootrootroot@thebookofinternet-db-instance.cx9rgbotmuld.ap-northeast-2.rds.amazonaws.com:3306/development'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{os.environ['db_id']}:{os.environ['db_pass']}@thebookofinternet-db-instance.cx9rgbotmuld.ap-northeast-2.rds.amazonaws.com:3306/development'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 limiter = Limiter(app, key_func=get_remote_address)
