9081b297005762aa952faa426be411218cc2e942
Pilsang Kim <pilsang0512@gmail.com>
2020-09-07 23:09:38 +0900

    fixed quote char

--- thebookofinternet/__init__.py
+++ thebookofinternet/__init__.py
@@ -6,7 +6,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{os.environ['db_id']}:{os.environ['db_pass']}@thebookofinternet-db-instance.cx9rgbotmuld.ap-northeast-2.rds.amazonaws.com:3306/development'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'''mysql+pymysql://{os.environ['db_id']}:{os.environ['db_pass']}@thebookofinternet-db-instance.cx9rgbotmuld.ap-northeast-2.rds.amazonaws.com:3306/development'''
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 limiter = Limiter(app, key_func=get_remote_address)
