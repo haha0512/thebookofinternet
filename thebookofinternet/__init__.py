2b4b7482774bfffa4204e0e68b2b20db321473a2
Pilsang Kim <pilsang0512@gmail.com>
2020-09-08 20:51:10 +0900

    fixed getting environment variable

--- thebookofinternet/__init__.py
+++ thebookofinternet/__init__.py
@@ -1,12 +1,10 @@
-import os
-
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'''mysql+pymysql://{os.environ['db_id']}:{os.environ['db_pass']}@thebookofinternet-db-instance.cx9rgbotmuld.ap-northeast-2.rds.amazonaws.com:3306/development'''
+app.config['SQLALCHEMY_DATABASE_URI'] = f'''mysql+pymysql://{request.environ['db_id']}:{request.environ['db_pass']}@thebookofinternet-db-instance.cx9rgbotmuld.ap-northeast-2.rds.amazonaws.com:3306/development'''
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 limiter = Limiter(app, key_func=get_remote_address)
